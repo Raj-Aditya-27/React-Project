865aa28c820d7a7c688ddc1cc58657058dee3940
Aditya Raj <138434890+Raj-Aditya-27@users.noreply.github.com>
2024-05-19 23:25:21 +0530

    Logic added

--- Budget-App/src/Components/AddExpenseForm.jsx
+++ Budget-App/src/Components/AddExpenseForm.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddExpenseForm() {
+
+  const [text, setText] = useState("");
+  const [amount,setAmount]=useState();
+
   return (
     <form>
       <div className="row">
         <div className="col-sm">
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" required className="form-control" />
+          <input type="text" value={text} onChange={(e)=>setText(e.target.value)} id="name" required className="form-control" />
         </div>
         <div className="col-sm">
           <label htmlFor="cost">Cost</label>
-          <input type="text" id="cost" required className="form-control" />
+          <input type="text" value={amount} onChange={(e)=>setAmount(e.target.value)} id="cost" required className="form-control" />
         </div>
         <div className="col-sm mt-4">
           <button type="submit" className="btn btn-primary">Save</button>
